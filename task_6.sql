61f2c967b1977e4254512f15ad622cd5f52df217
ABD-EL-ILAH <aeibouyhy@gmail.com>
2024-07-26 23:13:44 +0100

    Add task_6.sql to insert multiple rows into the customer table

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,7 @@
+-- Use the database
 USE alx_book_store;
 
+-- Insert multiple rows into the customer table
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),

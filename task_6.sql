65e7598d7a05d798314391975048d4befd4548de
ABD-EL-ILAH <aeibouyhy@gmail.com>
2024-07-27 18:58:16 +0100

    inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,9 @@
 -- Use the database
 USE alx_book_store;
 
--- Insert multiple rows into the customer table
-INSERT INTO Customers (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')
-INSERT INTO Customers (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.')
-INSERT INTO Customers (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+-- Insert multiple rows into the Customers table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
